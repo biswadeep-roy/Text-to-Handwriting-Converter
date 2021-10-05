10bfb13a9a42b459b7e8221df6abe6fd5c5c7697
Biswadeep Roy <74821633+thebiebs@users.noreply.github.com>
2021-10-05 20:40:13 +0530

    added automessenger

---
+++ AutoMessanger.py
@@ -0,0 +1,9 @@
+from typing import Optional
+import pyautogui, time
+time.sleep(0)
+for i in range(1000) :
+    x="Sun "
+    time.sleep(0)
+    pyautogui.typewrite(x)
+    pyautogui.press("enter")
+
