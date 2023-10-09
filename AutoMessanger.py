79d14e5c78c7ea63604e38748c2cfebf5fb84a05
Biswadeep Roy <biswadeeproy1230@gmail.com>
2023-10-09 19:22:28 +0530

    update

--- AutoMessanger.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-import pyautogui, time
-time.sleep(0)
-for i in range(100) :
-    x="Your Message"
-    time.sleep(0)
-    pyautogui.typewrite(x)
-    pyautogui.press("enter")
-
